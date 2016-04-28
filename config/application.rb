fd97540bd99fa4b8a9bd668ec023d52c17997277
Dmitry Ovechkin <dmitry.ovechkin@mocomedia.ru>
2016-04-28 16:58:58 +0300

    beautiful page

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require File.expand_path('../boot', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,6 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
